a0258e168f85ccc960b0174a9063f0dd4f964c71
Kieronm <kieron.j.moorcroft95@gmail.com>
2016-07-03 17:52:26 +0100

    Added index and details views for customers and movies.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -44,16 +44,18 @@ namespace Vidly.Controllers
 
         public ActionResult Index(int? pageIndex, string sortBy)
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+            var movies = GetMovies();
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                  new Movie { Id = 1, Name = "Shrek" },
+                  new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
